d75a05647492ef139af7f7b359e7e19a93f2f420
Polina Igrushkina <polina.igrushkina@gmail.com>
2022-09-06 00:43:43 +0300

    Solved task-02

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,18 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ArrayOFLI = ingredients.map(ingridient => {
+  const liEl = document.createElement("li");
+  liEl.textContent = ingridient;
+  liEl.classList.add('item');
+  return liEl;
+});
+
+const ingredientsEl = document.querySelector('#ingredients');
+
+ingredientsEl.append(...ArrayOFLI);
+
+console.log(ingredientsEl);
+
+
