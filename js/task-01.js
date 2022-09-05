03d39c20d6f24e9a2b3e4899da9e154c87616023
Polina Igrushkina <polina.igrushkina@gmail.com>
2022-09-05 23:59:48 +0300

    Solved task-01

---
+++ js/task-01.js
@@ -0,0 +1,26 @@
+const ulEl = document.querySelector('ul');
+
+const arrayOfMainLi = ulEl.querySelectorAll('.item');
+
+console.log(`Number of categories: ${arrayOfMainLi.length}`);
+
+for (const mainLi of arrayOfMainLi) {
+
+  const h2El = mainLi.querySelector('h2');;
+  const ulSecondaryEl = mainLi.querySelector('ul')
+  const arrayOfSecondaryLi = ulSecondaryEl.children;
+
+  console.log(`Category: ${h2El.textContent}`);
+  console.log(`Elements: ${arrayOfSecondaryLi.length}`);
+
+}
+
+
+
+
+
+
+
+
+
+
