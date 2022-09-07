dd81641daafb7e5c0f2d1485094423513b08e3a4
Polina Igrushkina <polina.igrushkina@gmail.com>
2022-09-07 13:26:52 +0300

    Solved task-08

--- js/task-08.js
+++ js/task-08.js
@@ -1,23 +1,3 @@
-// Напиши скрипт управления формой логина.
-
-// <form class="login-form">
-//   <label>
-//     Email
-//     <input type="email" name="email" />
-//   </label>
-//   <label>
-//     Password
-//     <input type="password" name="password" />
-//   </label>
-//   <button type="submit">Login</button>
-// </form>
-
-
-// Если пользователь заполнил все поля и отправил форму, собери значения полей в обьект, 
-//где имя поля будет именем свойства, а значение поля - значением свойства.
-//Для доступа к элементам формы используй свойство elements.
-// Выведи обьект с введенными данными в консоль и очисти значения полей формы методом reset.
-
 const formEl = document.querySelector('.login-form');
 
 formEl.addEventListener('submit', formSubmission);
