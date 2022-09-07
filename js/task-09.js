9e010bf112a1cdcd1093fff9ba615f95bcd05319
Polina Igrushkina <polina.igrushkina@gmail.com>
2022-09-07 14:01:20 +0300

    Solved task-09

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,18 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+};
+
+console.log(getRandomHexColor());
+
+const span = document.querySelector('.color');
+
+const button = document.querySelector('.change-color');
+
+button.addEventListener('click', changeColor);
+
+function changeColor(event) {
+  document.body.style.backgroundColor = getRandomHexColor();
+  span.textContent = getRandomHexColor();
+};
